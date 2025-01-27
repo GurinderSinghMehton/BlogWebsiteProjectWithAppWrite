e5e162894d1d48d993dd9970c32d3c982552e0b5
Gurinder Singh <156795784+GurinderSinghMehton@users.noreply.github.com>
2025-01-27 16:32:59 +0530

    Update PostCard.jsx

--- BlogWebsiteProject/src/components/PostCard.jsx
+++ BlogWebsiteProject/src/components/PostCard.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom'
 function PostCard({$id, title, featuredImage}) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
+        <div className='w-full bg-white rounded-3xl p-4'>
             <div className='w-full justify-center mb-4'>
                 <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
             </div>
-            <h2 className='text-2xl font-bold'>{title}</h2>
+            <h2 className='text-2xl font-bold mb-3'>{title}</h2>
         </div>
     </Link> 
   )
